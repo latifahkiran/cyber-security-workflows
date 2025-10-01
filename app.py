99a7b31a7704a257fdf68cb1151141910024ee80
latifahkiran <latifah.nisa@unmer.ac.id>
2025-10-01 21:03:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,5 @@
-from flask import Flask, request
-import sqlite3
-
-app = Flask(__name__)
-
-# coba alert flask debug
-app.config["DEBUG"] = True  
-
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-
-    # 🚨 Celah 2: SQL Injection
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
-    cursor.execute(query)  # rawan SQL injection
-    result = cursor.fetchall()
-    return str(result)
+def hello():
+    print("Hello, Cyber Security World!")
 
 if __name__ == "__main__":
-    app.run()
+    hello()
